6d1d682e544bb42e7b6af67abaf6f49777f5dbea
Gabriel <you@example.com>
2023-05-31 15:02:16 -0300

    ajustes

--- architecture.js
+++ architecture.js
@@ -1,26 +1,17 @@
 class Architecture {
     constructor() {
       // Functional Units
-      this.aFu_add = 3;
-      this.aFu_mult = 3;
-      this.aFu_store = 3;
-      this.aFu_load = 3;
+      this.fus = {
+        "Add": 3,
+        "Mult": 3,
+        "Store": 3,
+        "Load": 3
+      };
 
-      // Cycles per add/sub 
-      this.add_time = 3 
-
-      // mult/div
-      this.md_time = 3 
-
-      //load/store
-      this.ls_time = 2
-    }
-
-    initializeFu(){
-      this.fu_add = []
-      for (i=0; i<this.aFu_add;i++) {
-        this.fu_add.append({ state: 'Free' });
-      }
+      // Cycles per instruction
+      this.add_time = 3;
+      this.md_time = 3; 
+      this.ls_time = 2;
     }
 }
   
--- front.js
+++ front.js
@@ -1,17 +1,42 @@
 
 $(function(){
     $("#execucao").hide();
-    adicionar_linha();
+    add_row();
+    load_registers();
   });
 
 function execute(){
     $("#tela_inicial").hide();
     $("#execucao").show();
+    arq = new Architecture();
+    load_fu_table(arq);
+    console.log(arq.aFu_add)
 }
 
-function voltar(){
+function load_fu_table(arq){
+    var fus = arq.fus;
+    var tableBody = $("#fu_table tbody"); // Get the reference to the table body
+    
+    for (var key in fus) {
+        console.log(key)
+        console.log(fus[key])
+        for(i=0;i<fus[key];i++){
+            var newRow = $("<tr>"); // Create a new row with data
+            var nameCell = $("<td>").text(`${key}${i}`);
+            var stateCell = $("<td>").text("Free");
+            newRow.append(nameCell, stateCell);
+            tableBody.append(newRow); // Append the new row to the table body
+        }
+    }
+}
+
+function home(){
     $("#tela_inicial").show();
     $("#execucao").hide();
+}
+
+function next(){
+    console.log("next step")
 }
 
 function importFile(){
@@ -33,18 +58,16 @@ function importFile(){
     for (let i = 0; i < lines.length; i++) {
       const line = lines[i].trim();
       // Do something with each line of the file
-      console.log(line);
     }
   };
 
   reader.readAsText(file);
-  
+
 }
 
-function limpar(){
-    var rows = document.querySelectorAll(".intruction_row");
+function clear_table(){
+    var rows = document.querySelectorAll(".instruction_row");
     var length = rows.length
-    console.log(length)
     for(i = 2; i<length; i++){
         $("#table_body").find("#instruction_"+i).remove();
     }
@@ -55,15 +78,26 @@ function onchange_instruction(elemento){
     var selectedValue =  $(elemento).val(); // Obtém o valor do elemento selecionado
     var id_elemento = $(elemento).attr("id");
     var id = id_elemento.replace(/\D/g, "");
-    console.log(id)
     if(selectedValue == 6){
         $("#register3_"+id).prop("disabled", true);
+        $("#register3_"+id).attr("placeholder", "");
     }else{
         $("#register3_"+id).prop("disabled", false);
     }
 }
 
-function adicionar_linha(){
+function load_registers(){
+    
+    for(i=0;i<15;i++){
+        // Registers table (exec page)
+        var new_line = $("#register_name").clone();
+        new_line.find(".register_name").text("R"+i); // Atualiza index
+        new_line.appendTo("#registers_table_body");
+        new_line.removeAttr("hidden");
+    }
+}
+
+function add_row(){
     var nova_lina = $("#instuction").clone();
 
     var index = parseInt($("#row_count").val()) + 1; // Pegar e incrementar index
