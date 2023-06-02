0923571f316221ef1a2e1e7deab3afada0e8bf50
Gabriel <gabrielgpaiva@outlook.com>
2023-06-02 13:20:32 -0300

    changing logic, ajustments

--- architecture.js
+++ architecture.js
@@ -1,13 +1,22 @@
 class Architecture {
     constructor() {
       // Functional Units
-      this.fus = {
+      this.fus_cfg = {
         "Add": 2,
         "Mult": 2,
         "Div": 2,
         "Store": 2,
         "Load": 2
       };
+
+      this.fus = [] // Create functional units array
+      for(var key in this.fus_cfg){
+        for(i=0; i<this.fus_cfg[key];i++){
+          var new_key = key + i;
+          var value = "Free";
+          this.fus[new_key] = value;
+        }
+      }
 
       // Cycles per instruction
       this.add_time = 3;
--- tomasulo.js
+++ tomasulo.js
@@ -42,23 +42,32 @@ class Tomasulo {
     next(){
         this.state += 1; // Próximo estado (ciclo)
 
-        for (i=0; i<this.instr_ammount; i++){
-            var dependencia = false;
-            if(this.instructions_state[i] == "issue"){
+        for (i=0; i<this.instr_ammount; i++){ // Loop que percorre todas as intstruções
+            var dependencia = false; // Para identificar se há dependencia de dados
+            if(this.instructions_state[i] == "issue"){ // Se ainda não foi executada
 
-                if(i!=0){
-                    // Verificar se há dependencia de dados
+                if(i!=0){ // Na primeira instrução não há necescidade de verificar dependência
+                    // Verificar se há dependencia de dados (RAW, WAW, WAR)
                     console.log(this.instructions[i][1])
                     dependencia = false;
                 }
 
-                if(dependencia){
+                if(dependencia){ // Se há dependência, a instrução deverá aguardar
                     this.instructions_state[i] = "awaiting"
                     break;
-                }else{
+                }else{ // Se não
                     var instruction = this.instructions[i][0];
-                    var fu = this.getFu(instruction);
-                    for(i=0; i<this.ammount_fu; i++){
+                    var fu_name = this.getFu(instruction); // Obter unidade funcional da instrução
+                    for (var key in this.fus) {
+                        if (key.includes(fu_name) && this.fus[key] == "Free") {
+                          this.instructions_state[i] = "execute"; // passar instrução para execute
+                          this.fus[key] = "busy";
+                          //$("#fu_state_"+(i+1)).text("Busy");
+                          break;
+                        }
+                      }
+                    /*
+                    for(i=0; i<this.ammount_fu; i++){ // Percorrer unidades funcionais
                         var table_instr_name = $("#fu_name_"+(i+1)).text();
                         table_instr_name = table_instr_name.split('').slice(0, 3).join('');
                         var state = $("#fu_state_"+(i+1)).text();
@@ -69,11 +78,12 @@ class Tomasulo {
                             break;
                         }
                     }
+                    */
                     break;
                 }
 
             }
         }
-        update_tables(this.state, this.instructions_state, this.instr_ammount);
+        update_tables(this.state, this.instructions_state, this.instr_ammount, this.fus);
     }
 }
